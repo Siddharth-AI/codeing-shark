f7e982dc82bb0e1d4726591e1a80ffa38f2aa7db
Siddharth-AI <sidshah895@gmail.com>
2025-10-11 13:15:00 +0530

    miner chnage

--- src/app/lib/Images.ts
+++ src/app/lib/Images.ts
@@ -1,7 +1,7 @@
 import homeTestimonialImage1 from '@/assets/images/home/testimonial/testimonials1.jpeg';
 import homeTestimonialImage2 from '@/assets/images/home/testimonial/testimonials2.png';
 import homeTestimonialImage3 from '@/assets/images/home/testimonial/testimonials3.png';
-import homeTestimonialImage4 from '@/assets/images/home/testimonial/testimonials3.png';
+import homeTestimonialImage4 from '@/assets/images/home/testimonial/testimonials4.png';
 import courseTestimonialImage1 from '@/assets/images/course/testimonial/testimonials1.png';
 import courseTestimonialImage2 from '@/assets/images/course/testimonial/testimonials2.png';
 
